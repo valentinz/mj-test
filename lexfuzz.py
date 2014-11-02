17b78d6002bd914ffd8503cbee61cbfa7652f47a
Max Wagner <max@trollbu.de>
2014-11-02 12:59:15 +0100

    Improve fuzzer
    you can now use --aggressive to catch some edge cases the normal mode
    doesn't generate test cases for.

--- lexfuzz.py
+++ lexfuzz.py
@@ -1,6 +1,13 @@
 #! /usr/bin/env python
 """Prints a randomly generated string to stdout, and the corresponding
-list of tokens (in the format --lextest produces) to stderr"""
+list of tokens (in the format --lextest produces) to stderr.
+
+Usage: lexfuzz.py [LENGTH | --aggressive]
+
+  LENGTH        Average length (in tokens) of generated test cases
+  --aggressive  use a more aggressive mode (which may produce an incorrect reference
+                token stream)
+"""
 
 from enum import Enum
 import random
@@ -12,7 +19,19 @@ keywords = ["abstract", "assert", "boolean", "break", "byte", "case", "catch", "
 
 operators = [':', '!=', '!', '(', ')', '*=', '*', '++', '+=', '+', ',', '-=', '--', '-', '.', '/=', '/', ':', ';', '<<=', '<<', '<=', '<', '==', '=', '>=', '>>=', '>>>=', '>>>', '>>', '>', '?', '%=', '%', '&=', '&&', '&', '[', ']', '^=', '^', '{', '}', '~', '|=', '||', '|']
 
-needs_make_whitespace = [('!','='), ('*','='), ('+','+'), ('+','='), ('-','='), ('-','-'), ('/','='), ('<<','='), ('<','<'), ('<','='), ('=','='), ('>','='), ('>>','='), ('>>>','='), ('>','>>'), ('>>','>'), ('>','>'), ('%','='), ('&','='), ('&','&'), ('^','='), ('|','=')]
+needs_whitespace = [('/', '*='), ('/', '*'), ('*', '/'), ('!=', '='), ('!', '=='), ('!', '='), ('*=', '='), ('*', '=='), ('*', '='), ('++', '+'), ('++', '='), ('+=', '='), ('+', '++'), ('+', '+='), ('+', '+'), ('+', '=='), ('+', '='), ('-=', '='), ('--', '-'), ('--', '='), ('-', '-='), ('-', '--'), ('-', '-'), ('-', '=='), ('-', '='), ('/=', '='), ('/', '=='), ('/', '='), ('<<=', '='), ('<<', '<='), ('<<', '<'), ('<<', '=='), ('<<', '='), ('<=', '='), ('<', '<<='), ('<', '<<'), ('<', '<='), ('<', '<'), ('<', '=='), ('<', '='), ('==', '='), ('=', '=='), ('=', '='), ('>=', '='), ('>>=', '='), ('>>>=', '='), ('>>>', '=='), ('>>>', '='), ('>>>', '>='), ('>>>', '>>='), ('>>>', '>>'), ('>>>', '>'), ('>>', '=='), ('>>', '='), ('>>', '>='), ('>>', '>>='), ('>>', '>>>='), ('>>', '>>>'), ('>>', '>>'), ('>>', '>'), ('>', '=='), ('>', '='), ('>', '>='), ('>', '>>='), ('>', '>>>='), ('>', '>>>'), ('>', '>>'), ('>', '>'), ('%=', '='), ('%', '=='), ('%', '='), ('&=', '='), ('&&', '='), ('&&', '&'), ('&', '=='), ('&', '='), ('&', '&='), ('&', '&&'), ('&', '&'), ('^=', '='), ('^', '=='), ('^', '='), ('|=', '='), ('||', '='), ('||', '|'), ('|', '=='), ('|', '='), ('|', '|='), ('|', '||'), ('|', '|')]
+
+def make_needs_whitespace():
+    # this algorithm runs in O(yolo)
+    global needs_whitespace
+    needs_whitespace = [('/', '*='), ('/', '*'), ('*', '/')]  # since comment start and end are special tokens, this needs to be hardcoded
+    for kw1 in operators:
+        for kw2 in operators:
+            for cand in operators + ['']:
+                for cand2 in operators + ['']:
+                    if cand+cand2 == kw1 + kw2 and (cand != kw1 or cand2 != kw2) and (kw1,kw2) not in needs_whitespace:
+                        needs_whitespace.append((kw1,kw2))
+
 
 class Token(Enum):
     T_WHITESPACE = 0
@@ -22,7 +41,7 @@ class Token(Enum):
     T_OPERATOR = 4
 
 WHITESPACE_PROB = 50 # percent
-STRING_LENGTH = 30
+STRING_LENGTH = 5
 COMMENT_PROB = 5
 COMMENT_LENGTH = 10
 COMMENT_NESTING_PROB = 10 # percent
@@ -31,7 +50,11 @@ def _make_comment():
     length = random.randrange(COMMENT_LENGTH)
     c = "/*"
     while random.randrange(100) < COMMENT_NESTING_PROB:
-        c += get_string(length, False) + "/*"
+        c += get_string(length, False)
+        if c[-1] == '*':
+            # avoid accidentally closing the comment
+            c += _make_whitespace(comments=False)
+        c += "/*" + _make_whitespace(comments=False)
     c += get_string(length, False) + "*/"
     return c
 
@@ -63,7 +86,7 @@ def _make_identifier():
         return str(random.randrange(20))
 
     if random.getrandbits(1):
-        ident += letter()
+        ident += random.choice(string.ascii_letters) if random.getrandbits(1) else '_'
     kw = random.choice(keywords)
     ident += kw
 
@@ -75,23 +98,21 @@ def _make_integer():
     return str(random.getrandbits(32))
 
 def _is_var(tok):
-    ident = r'[a-zA-Z0-9_]'
-    return re.match(ident, tok[0]) and re.match(ident, tok[-1])
+    c = string.ascii_letters + '0123456789_'
+    return tok[0] in c and tok[-1] in c
 
 
 
 
-def _pad_tokens(tokens, comments=True):
+def _pad_tokens(tokens, comments=True, pad_operators=True):
     """Some tokens need to be padded with whitespace, e.g. two consecutive keywords"""
     padded_tokens = []
     last_token = None
     for t_type, t in tokens:
         if last_token and _is_var(last_token) and _is_var(t):
             padded_tokens.append((Token.T_WHITESPACE, _make_whitespace(comments)))
-        else:
-            for tup in needs_make_whitespace:
-                if last_token and last_token == tup[0] and t == tup[1]:
-                    padded_tokens.append((Token.T_WHITESPACE, _make_whitespace(comments)))
+        elif ((last_token, t) in needs_whitespace) and pad_operators:
+            padded_tokens.append((Token.T_WHITESPACE, _make_whitespace(comments)))
         padded_tokens.append((t_type, t))
 
         last_token = t
@@ -117,10 +138,12 @@ def get_tokens(length):
         tokens.append(_random_token())
     return tokens
 
+
+
 def get_string(length, comments=True):
     return ''.join([t[1] for t in _pad_tokens(get_tokens(length), comments)])
 
-def t_str(token):
+def lextest_str(token):
     if token[0] == Token.T_IDENT:
         return "identifier " + token[1]
     if token[0] == Token.T_INTEGER:
@@ -129,9 +152,19 @@ def t_str(token):
 
 if __name__ == '__main__':
     length = STRING_LENGTH
-    if len(sys.argv) > 1:
-        length = int(sys.argv[1])
+    pad_operators = True
+    comments = True
+    if '--aggressive' in sys.argv:
+        # don't use smart token padding logic
+        # this will run into some edge cases the normal mode avoids, but has
+        # the disadvantage that its results have to be manually inspected
+        pad_operators = False
+        comments = False
+        length = 10
+    else:
+        if len(sys.argv) > 1:
+            length = int(sys.argv[1])
 
     tokens = get_tokens(length)
-    print(''.join([t[1] for t in _pad_tokens(tokens)]), file=sys.stdout)
-    print('\n'.join([t_str(t) for t in tokens]) + '\n', file=sys.stderr)
+    print(''.join([t[1] for t in _pad_tokens(tokens, comments=comments, pad_operators=pad_operators)]), file=sys.stdout)
+    print('\n'.join([lextest_str(t) for t in tokens] + ['EOF']) + '\n', file=sys.stderr)
